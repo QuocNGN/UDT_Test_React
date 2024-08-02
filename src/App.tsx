f18c60504d9bc55b60f9aba18bd36b8bbeb1ee78
NQuocNk20 <quoc.nn00820@sinhvien.hoasen.edu.vn>
2024-08-02 16:01:28 +0700

    chore(jest): update Jest configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { FC } from 'react'
-import './assets/styles/app.css'
-import './assets/styles/app.scss'
+// Modified from the create-react-app TypeScript template
+// See: https://github.com/facebook/create-react-app/blob/main/packages/cra-template-typescript/template/src/App.tsx
+import React from 'react';
 
-const App: FC = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
+// import './app.css';
+
+// import logo from './icons/logo.svg';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="app" role="main">
+        <header className="app-header">
+          <p>
+            Edit <code>src/app.tsx</code> and save to reload.
+          </p>
+        <h1>Hello World</h1>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App
