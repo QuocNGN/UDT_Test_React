79b9fd357fd46782c201214da9f650a358644a79
NQuocNk20 <quoc.nn00820@sinhvien.hoasen.edu.vn>
2024-08-06 22:28:13 +0700

    feat: create initial UI Calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-// Modified from the create-react-app TypeScript template
-// See: https://github.com/facebook/create-react-app/blob/main/packages/cra-template-typescript/template/src/App.tsx
 import React from 'react'
-
+import Calculator from './Calculator'
 // import './app.css';
-
-// import logo from './icons/logo.svg';
-
 export default class App extends React.Component {
   render() {
     return (
-      <div className='app' role='main'>
-        <header className='app-header'>
-          <p>
-            Edit <code>src/app.tsx</code> and save to reload.
-          </p>
-          <h1>Hello World</h1>
-        </header>
+      <div className='App'>
+        <div className='icon-wrapper'>
+          <button className='icon Red'></button>
+          <button className='icon Yellow'></button>
+          <button className='icon Blue'></button>
+        </div>
+        <Calculator />
       </div>
     )
   }
---
+++ src/components/Input.tsx
@@ -0,0 +1,9 @@
+import '../assets/styles/app.scss'
+
+export default function Input() {
+  return (
+    <div className='input-container'>
+      <h1>0</h1>
+    </div>
+  )
+}
